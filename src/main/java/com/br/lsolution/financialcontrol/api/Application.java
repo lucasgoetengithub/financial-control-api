4f8182079378b051a32c0598a2aa4c27b4a9b774
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-03-30 19:37:16 -0300

    teste

--- src/main/java/com/br/lsolution/financialcontrol/api/Application.java
+++ src/main/java/com/br/lsolution/financialcontrol/api/Application.java
@@ -3,9 +3,11 @@ package com.br.lsolution.financialcontrol.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
