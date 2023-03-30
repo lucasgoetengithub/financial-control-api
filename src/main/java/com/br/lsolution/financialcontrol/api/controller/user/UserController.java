c36742000a1f47a64ee81e4c943e7f8ae5740106
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-03-30 19:28:19 -0300

    teste

--- src/main/java/com/br/lsolution/financialcontrol/api/controller/user/UserController.java
+++ src/main/java/com/br/lsolution/financialcontrol/api/controller/user/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.annotation.Resource;
+
+@Resource(name="user")
 @RequestMapping(value ="/users")
 public class UserController {
 
