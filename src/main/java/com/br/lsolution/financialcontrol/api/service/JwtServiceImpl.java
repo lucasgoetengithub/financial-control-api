5baee8050daa15dff0991dd373fb14942f05c6f6
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-03-30 20:01:16 -0300

    teste

---
+++ src/main/java/com/br/lsolution/financialcontrol/api/service/JwtServiceImpl.java
@@ -0,0 +1,78 @@
+package com.br.lsolution.financialcontrol.api.service;
+
+import com.br.lsolution.financialcontrol.api.model.user.Users;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    @Value("${jwt.expiracao}")
+    private String expiracao;
+
+    @Value("${jwt.chave-assinatura}")
+    private String chaveAssinatura;
+
+    @Override
+    public String gerarToken(Users usuario) {
+        long exp = Long.valueOf(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(exp);
+        Instant instant = dataHoraExpiracao.atZone( ZoneId.systemDefault() ).toInstant();
+        java.util.Date data = Date.from(instant);
+
+        String horaExpiracaoToken = dataHoraExpiracao.toLocalTime()
+                .format(DateTimeFormatter.ofPattern("HH:mm"));
+
+        String token = Jwts
+                .builder()
+                .setExpiration(data)
+                .setSubject(usuario.getEmail())
+                .claim("userid", usuario.getId())
+                .claim("nome", usuario.getName())
+                .claim("horaExpiracao", horaExpiracaoToken)
+                .signWith( SignatureAlgorithm.HS512 , chaveAssinatura )
+                .compact();
+
+        return token;
+    }
+
+    @Override
+    public Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    @Override
+    public boolean isTokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            java.util.Date dataEx = claims.getExpiration();
+            LocalDateTime dataExpiracao = dataEx.toInstant()
+                    .atZone(ZoneId.systemDefault()).toLocalDateTime();
+            boolean dataHoraAtualIsAfterDataExpiracao = LocalDateTime.now().isAfter(dataExpiracao);
+            return !dataHoraAtualIsAfterDataExpiracao;
+        }catch(ExpiredJwtException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String obterLoginUsuario(String token) {
+        Claims claims = obterClaims(token);
+        return claims.getSubject();
+    }
+
+}
