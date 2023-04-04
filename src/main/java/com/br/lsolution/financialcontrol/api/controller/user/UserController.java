de346c879fe5b66941f22fc114fb541286950ec5
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-04-03 22:43:12 -0300

    permissoa usuario

--- src/main/java/com/br/lsolution/financialcontrol/api/controller/user/UserController.java
+++ src/main/java/com/br/lsolution/financialcontrol/api/controller/user/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value ="/users")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
