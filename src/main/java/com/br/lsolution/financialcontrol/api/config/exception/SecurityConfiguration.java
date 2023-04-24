d44b87a6102bf238da19e4d3ebed72b5eed49668
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-04-24 17:30:29 -0300

    teste

--- src/main/java/com/br/lsolution/financialcontrol/api/config/exception/SecurityConfiguration.java
+++ src/main/java/com/br/lsolution/financialcontrol/api/config/exception/SecurityConfiguration.java
@@ -74,28 +74,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter(){
-
-        List<String> all = Arrays.asList("*");
-
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedMethods(all);
-        config.setAllowedOrigins(all);
-        config.setAllowedHeaders(all);
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        CorsFilter corFilter = new CorsFilter(source);
-
-        FilterRegistrationBean<CorsFilter> filter =
-                new FilterRegistrationBean<CorsFilter>(corFilter);
-        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return filter;
-    }
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilter(){
+//
+//        List<String> all = Arrays.asList("*");
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedMethods(all);
+//        config.setAllowedOrigins(all);
+//        config.setAllowedHeaders(all);
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        CorsFilter corFilter = new CorsFilter(source);
+//
+//        FilterRegistrationBean<CorsFilter> filter =
+//                new FilterRegistrationBean<CorsFilter>(corFilter);
+//        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//
+//        return filter;
+//    }
 
 
 
